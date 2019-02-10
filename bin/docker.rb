90e12b26c2e8acb297f101c5bfabf55042be5a89
nak1114 <naktak1114@gmail.com>
2019-02-11 02:38:27 +0900

    Created test env with docker.

---
+++ bin/docker.rb
@@ -0,0 +1,13 @@
+root=File.expand_path('../../', __FILE__)
+if ARGV[0]=="clean"
+  system"docker container prune"
+  system"docker image prune"
+  exit 0
+end
+Dir.chdir(root) do
+  system %q(docker build -t pigpio -f ./bin/Dockerfile .)
+  docker_run=%(docker run --rm -it -v "#{root}":/myapp -w /myapp #{ENV['DOCKER_OPT']} pigpio)
+  system("#{docker_run} cp -pf /tmp/Gemfile.lock /myapp")
+  system"#{docker_run} /bin/bash"
+end
+__END__
---
+++ spec/ext/extconf.rb
@@ -0,0 +1,2 @@
+require 'mkmf'
+create_makefile('libpigpiod_if2')
